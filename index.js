8e03bf3aa1024301d3b75953fea21ffd1a346a45
storm <stormarasa2@gmail.com>
2023-10-04 11:18:40 +0300

    Modified DOM elements

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const main = document.querySelector('main')
+main.remove();
+
+ const newHeader = document.createElement('h1')
+ newHeader.id = 'victory';
+ newHeader.innerHTML= "Storm is the champion!"
+
+
+//newHeader.nodeName = `h1`
+// const newH1 = document.createElement('h1')
+// newH1.id = 'victory'
+// newHeader.appendChild = (newH1)
+//document.appendChild(newHeader)
+//h1.textContent = 'Victory'
+//body.innerHTML = ``
